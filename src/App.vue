860fb6b962aae8bbde2588e246b0f4b324cb5e0f
Dianne <dianne.diazm@gmail.com>
2020-10-28 19:53:00 -0300

    segundo commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
-    <Formulario />
     <Pokeguia />
   </div>
 </template>
 
 <script>
-import Formulario from './components/Formulario.vue';
 import Pokeguia from './components/Pokeguia.vue';
 
 export default {
   name: 'App',
   components: {
-    Formulario,
     Pokeguia
   }
 }
