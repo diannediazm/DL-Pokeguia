134cbcc3eecb2b69c52ea5bdad14b294e6cba4bc
Dianne <dianne.diazm@gmail.com>
2020-10-28 16:17:56 -0300

    primer commit

---
+++ src/components/Pokeguia.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pokeguia',
+  data () {
+    return {
+      imagen: "",
+      movimientos: "",
+      habilidades: ""
+    }
+  },
+  methods: {
+    beforeCreate() {
+      console.log("before Create : ", this.message);
+      },
+    created() {
+      fetch("https://pokeapi.co/api/v2/pokemon/")
+      .then(response => response.json())
+      .then(json => console.log(json));
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
